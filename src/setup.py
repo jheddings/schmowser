9f73fb9326823e1d46aa8a7b5ca1ac5f60a5247b
jheddings <jason@heddings.com>
2019-11-11 23:30:58 -0700

    Additional options for packaging

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,13 @@
 
 from setuptools import setup
 
+# this is expecting to run from the base directory of the project
+
 setup(
+    name='Schmowser',
+    description='Utility for redirecting links to a user-specified browser...  a.k.a browser-shmowser',
+    url='https://github.com/jheddings/schmowser',
+    license='MIT',
     app=['src/schmowser.py'],
     setup_requires=['py2app'],
 )
