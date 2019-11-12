1043deeb5582db1b59509eab711cc52b9a4e8c79
jheddings <jason@heddings.com>
2019-11-11 21:30:14 -0700

    Refactored app object

--- src/schmowser.py
+++ src/schmowser.py
@@ -1,131 +1,192 @@
 #!/usr/bin/env python2.7
 
-import yaml
-import argparse
+import os
 
 import logging
 import logging.config
 
-import re
-import subprocess
+################################################################################
+class Schmowser():
 
-from os import path
+    #---------------------------------------------------------------------------
+    def __init__(self):
+        self.logger = logging.getLogger('schmowser')
 
-#-------------------------------------------------------------------------------
-# read command line options
-argp = argparse.ArgumentParser(description='schmowser: an app routing utility')
+        self.apps = { }
+        self.handlers = { }
 
-# TODO correct the default config path...
-argp.add_argument('--config', default='.schmowserc',
-                  help='configuration file (default: ~/.schmowserc)')
+        self.default_app_name = None
+        self.dry_run = False
 
-argp.add_argument('--debug', default=False, action='store_true',
-                  help='enable debugging mode (default: False)')
+    #---------------------------------------------------------------------------
+    def add_app(self, app_name, app_path):
+        self.logger.debug('adding app: %s = %s', app_name, app_path)
 
-argp.add_argument('params', nargs=argparse.REMAINDER)
+        if not os.path.exists(app_path):
+            self.logger.error('invalid app path: %s', app_path)
+            return False
 
-args = argp.parse_args()
-gconf = { }
+        old_path = self.apps.pop(app_name, None)
+        if old_path is not None:
+            self.logger.debug('overriding previous app: %s', old_path)
 
-#-------------------------------------------------------------------------------
-# load user configuration options
-with open(args.config) as config_file:
-    gconf = yaml.load(config_file, Loader=yaml.CLoader)
+        self.apps[app_name] = app_path
 
-# TODO error checking on config file structure
+    #---------------------------------------------------------------------------
+    def add_handler(self, expr, app_name):
+        self.logger.debug('adding handler: %s = %s', expr, app_name)
 
-# load configuration options
-options = gconf['Options']
+        if app_name not in self.apps:
+            self.logger.error('invalid app name: %s', app_name)
+            return False
 
-# configure logging and setup debug options
-if args.debug is True:
-    logging.basicConfig(level=logging.DEBUG)
+        old_name = self.handlers.pop(expr, None)
+        if old_name is not None:
+            self.logger.debug('overriding previous handler: %s', old_name)
 
-elif 'Logging' in gconf:
-    logging.config.dictConfig(gconf['Logging'])
+        self.handlers[expr] = app_name
 
-default_app = options['DefaultApp']
-logging.debug('[conf] default app: %s', default_app)
+    #---------------------------------------------------------------------------
+    # find the app for a given input parameter
+    def get_app_name(self, param):
+        import re
 
-logging.debug('[conf] config file loaded: %s', args.config)
+        self.logger.debug('retrieving app for: %s', param)
+
+        app_name = None
+
+        # look through each handler to find a match
+        for expr,name in self.handlers.items():
+            self.logger.debug('-- checking: %s', expr)
+            m = re.match(expr, param)
+
+            # XXX should we stop on a match or let the last one win?
+            if m is not None:
+                app_name = name
+                self.logger.debug('-- match FOUND: %s', app_name)
+                break
+
+        # if nothing matched, use the default
+        if app_name is None:
+            app_name = self.default_app_name
+            self.logger.debug('-- match NOT FOUND, using default: %s', app_name)
+
+        return app_name
+
+    #---------------------------------------------------------------------------
+    # load the path to a given app
+    def get_app_path(self, app_name):
+        self.logger.debug('get path to app: %s', app_name)
+        app_path = self.apps.get(app_name, None)
+
+        if app_path is None:
+            self.logger.error('no path for app: %s', app_name)
+        else:
+            self.logger.debug('application path: %s', app_path)
+
+        return app_path
+
+    #---------------------------------------------------------------------------
+    def route(self, param):
+        self.logger.debug('processing input parameter: %s', param)
+
+        app_name = self.get_app_name(param)
+
+        self._launch(app_name, param)
+
+    ################################################################################
+    # launch the given application
+    def _launch(self, app_name, *argv):
+        import subprocess
+
+        self.logger.info('Launching App: %s -- %s', app_name, ','.join(argv))
+
+        app_path = self.get_app_path(app_name)
+        if app_path is None:
+            self.logger.error('invalid app: %s', app_name)
+            return False
+
+        # TODO use shlex.quote on path and argv
+
+        # launch the correct application for the parameter
+        if self.dry_run is False:
+            # XXX should we care if the open call fails?  e.g. a bad app path
+            self.logger.debug('starting application process')
+            subprocess.call(['open', '-a', app_path, *argv])
+
+        return True
 
 ################################################################################
-# launch the given application
-def launch(app, *argv):
-    logging.info('Launching App: %s -- %s', app, ','.join(argv))
+def parse_args():
+    import argparse
 
-    app_path = get_app_path(app)
-    if app_path is None:
-        logging.error('invalid app: %s', app)
-        return
+    argp = argparse.ArgumentParser(description='schmowser: an app routing utility')
 
-    # TODO add DryRun option to global config
-    # TODO use shlex.quote on path and argv
+    # TODO correct the default config path...
+    argp.add_argument('--config', default='.schmowserc',
+                      help='configuration file (default: ~/.schmowserc)')
 
-    # launch the correct application for the parameter
-    if args.debug is False:
-        # XXX should we care if the open call fails?  e.g. a bad app path
-        logging.debug('starting application process')
-        subprocess.call(['open', '-a', app_path, *argv])
+    argp.add_argument('--debug', default=False, action='store_true',
+                      help='enable debugging mode (default: False)')
 
-    return True
+    argp.add_argument('params', nargs=argparse.REMAINDER)
+
+    return argp.parse_args()
 
 ################################################################################
-# load the path to a given app
-def get_app_path(app):
-    # global applications
-    apps = gconf['Applications']
+def load_config(args):
+    import yaml
 
-    if app not in apps:
-        logging.debug('-- no path for app: %s', app)
-        return None
+    with open(args.config) as config_file:
+        conf = yaml.load(config_file, Loader=yaml.CLoader)
 
-    # load the correct app
-    app_path = apps[app]
+    # TODO error checking on config file structure
 
-    if not path.exists(app_path):
-        logging.debug('-- invalid path: %s', app_path)
-        return None
+    # configure logging and setup debug options
+    if args.debug is True:
+        logging.basicConfig(level=logging.DEBUG)
 
-    logging.debug('-- application path: %s', app_path)
+    elif 'Logging' in conf:
+        logging.config.dictConfig(conf['Logging'])
 
-    return app_path
+    logging.debug('config file loaded: %s', args.config)
+
+    return conf
 
 ################################################################################
-# find the app for a given input parameter
-def get_app(param):
-    logging.debug('-- retrieving app for: %s', param)
+def configure_app(app, conf):
+    logging.debug('loading user configuration')
 
-    app = None
+    for app_name,app_path in conf['Applications'].items():
+        app.add_app(app_name, app_path)
 
-    # load global handlers
-    handlers = gconf['Handlers']
+    for path_expr,app_name in conf['Handlers'].items():
+        app.add_handler(path_expr, app_name)
 
-    # look through each handler to find a match
-    for handler in handlers:
-        logging.debug('-- checking: %s', handler)
-        m = re.match(handler, param)
+    options = conf['Options']
 
-        # XXX should we stop on a match or let the last one win?
-        if m is not None:
-            app = handlers[handler]
-            logging.debug('-- match FOUND: %s', app)
-            break
+    # TODO handle the case if they are not present in the config...
 
-    # if nothing matched, use the default
-    if app is None:
-        app = default_app
-        logging.debug('-- match NOT FOUND, using default: %s', app)
+    app.default_app_name = options['DefaultApp']
+    logging.debug('[conf] default app: %s', app.default_app_name)
 
-    return app
+    app.dry_run = options['DryRun']
+    logging.debug('[conf] dry run: %s', app.dry_run)
+
+################################################################################
+def main():
+    args = parse_args()
+    conf = load_config(args)
+
+    app = Schmowser()
+    configure_app(app, conf)
+
+    for param in args.params:
+        app.route(param)
 
 ################################################################################
 ## MAIN ENTRY
 
-# process each of the parameters
-for param in args.params:
-    logging.debug('processing input parameter: %s', param)
+if __name__ == '__main__':
+    main()
 
-    app = get_app(param)
-
-    launch(app, param)
