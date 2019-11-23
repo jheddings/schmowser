cd08fe1d2308c307763e4176629c95fdda5f3247
jheddings <jason@heddings.com>
2019-11-23 00:26:19 -0700

    Added basic test case

--- test/test_schmowser.py
+++ test/test_schmowser.py
@@ -14,3 +14,15 @@ class SchmowserObjectTest(unittest.TestCase):
         app = schmowser.Schmowser()
         self.assertIsNotNone(app.default_app_name)
 
+    #---------------------------------------------------------------------------
+    def test_BasicAppMatch(self):
+        app = schmowser.Schmowser()
+        app.add_app('Foo', '/usr/bin/true')
+        app.add_handler('^http://.*foo.*$', 'Foo')
+
+        good_name = app.get_app_name('http://www.foo.com/')
+        self.assertEqual(good_name, 'Foo')
+
+        bad_name = app.get_app_name('http://www.bar.com/')
+        self.assertNotEqual(bad_name, 'Foo')
+
