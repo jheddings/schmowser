ef9fbe44534e6e6a59032354bd7fa2f6c4ce6f24
jheddings <jason@heddings.com>
2019-11-26 12:05:25 -0700

    Added URL handlers to Info.plist

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
 # py2app setup file for schmowser
 
-from setuptools import setup
+import setuptools
 
 # this is expecting to run from the base directory of the project
 
-setup(
+# override app bundle metadata
+plist = dict(
+    CFBundleVersion='0.1.0',
+    CFBundleIdentifier='com.heddings.schmowser',
+    CFBundleURLTypes=[
+        dict(
+            CFBundleURLName='Web URLs',
+            CFBundleURLSchemes=['http', 'https']
+        ),
+        dict(
+            CFBundleURLName='Local URLs',
+            CFBundleURLSchemes=['file']
+        )
+    ]
+)
+
+# perform the bundling using setuptools with py2app
+setuptools.setup(
     name='Schmowser',
     description='Utility for redirecting links to a user-specified browser...  a.k.a browser-shmowser',
     url='https://github.com/jheddings/schmowser',
     license='MIT',
+
     app=['src/schmowser.py'],
     setup_requires=['py2app'],
+
+    options=dict(
+        py2app=dict(
+            plist=plist,
+        )
+    ),
 )
